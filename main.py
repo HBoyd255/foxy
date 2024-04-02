06756c6c7c388d755b66062fffa88988cac526c9
Harry Boyd <hboyd255@gmail.com>
2024-04-02 17:49:24 +0100

    Finished main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from modules.is_app_open import is_app_open_on_current_desktop
 
+import os
+import sys
+
+FIREFOX_DIRECTORY = "C:\\Program Files\\Mozilla Firefox"
 
 def main():
-    if is_app_open_on_current_desktop("firefox"):
-        print("Firefox is open on this virtual desktop.")
-    else:
-        print("Firefox is not open on this virtual desktop.")
+
+    # Change the current directory to the location of firefox.
+    os.chdir(FIREFOX_DIRECTORY)
+
+    # Combine the arguments received  by this script into one string.
+    arguments = " ".join(sys.argv[1:])
+
+    # If firefox is not open on the current desktop, add the new-window flag.
+    if not is_app_open_on_current_desktop("firefox"):
+        arguments += " --new-window"
+
+    # Open firefox with the arguments.
+    os.system(f"firefox.exe {arguments}")
 
 
 if __name__ == "__main__":
