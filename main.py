cbe9269418019feee389730613229c1de26e8d30
Harry Boyd <hboyd255@gmail.com>
2024-04-04 13:37:02 +0100

    Merged input arguments into single string

--- main.py
+++ main.py
@@ -9,28 +9,28 @@ FIREFOX_DIRECTORY = "C:\\Program Files\\Mozilla Firefox"
 
 def main():
 
-    # Change the current directory to the location of firefox.
+    # Change the current directory to the location of Firefox.
     os.chdir(FIREFOX_DIRECTORY)
 
+    # Get the arguments provided to the script.
     input_args = sys.argv[1:]
 
+    # If no arguments are provided, open a new tab.
     if len(input_args) == 0:
         input_args.append("about:newtab")
 
-    # If firefox is not open on the current desktop, open a new window.
+    # Join the input arguments into a single string.
+    joined_input_args = " ".join(input_args)
+
+    # If Firefox is not open on the current desktop, open a new window.
     if not is_app_open_on_current_desktop("firefox"):
         tab_or_window = "--new-window"
     else:
-        # If firefox is open on the current desktop, open a new tab.
+        # If Firefox is open on the current desktop, open a new tab.
         tab_or_window = "--new-tab"
 
-    firefox_args = ["firefox.exe"]
-    firefox_args.append(tab_or_window)
-    for i in input_args:
-        firefox_args.append(i)
-
-    # Open firefox with the arguments.
-    subprocess.Popen(firefox_args)
+    # Open Firefox with the arguments.
+    subprocess.Popen(["firefox.exe", tab_or_window, joined_input_args])
 
 
 if __name__ == "__main__":
